6ef168dfaec5f7fa885361ad88e73977c9a2855e
Sjoestroem <17090707+masj3107@users.noreply.github.com>
2025-02-04 14:02:53 +0000

    Rename to Strava_CR_play

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'MyFrontend' title`, () => {
+  it(`should have the 'Strava_CR_play' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyFrontend');
+    expect(app.title).toEqual('Strava_CR_play');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MyFrontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Strava_CR_play');
   });
 });
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:5267'
-  };
-  
+  production: false,
+  // other development-specific settings
+};
